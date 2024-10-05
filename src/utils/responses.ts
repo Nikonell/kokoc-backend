ac2f8560ae657d58d4e7a8395835627f7d0b27a6
Nikonell <rozhkovn08@gmail.com>
2024-10-05 12:46:50 +0700

    fix: remove t

--- src/utils/responses.ts
+++ src/utils/responses.ts
@@ -4,20 +4,20 @@ interface SetObject {
     status?: number | keyof StatusMap;
 }
 
-export const successResponse = <T>(set: SetObject, data: T, status = 200) => {
+export const successResponse = (set: SetObject, data: any, status = 200) => {
     set.status = status;
     return {
         status: "success",
         data,
-    } as { status: "success"; data: T };
+    } as { status: "success"; data: any };
 };
 
-export const validationErrorResponse = <T>(set: SetObject, data: T, status: number) => {
+export const validationErrorResponse = (set: SetObject, data: any, status: number) => {
     set.status = status;
     return {
         status: "validation_error",
         data,
-    } as { status: "validation_error"; data: T };
+    } as { status: "validation_error"; data: any };
 };
 
 export const errorResponse = (set: SetObject, message: string, status = 500) => {
@@ -28,7 +28,7 @@ export const errorResponse = (set: SetObject, message: string, status = 500) => 
     } as { status: "error"; message: string };
 };
 
-export const paginatedResponse = <T>(set: SetObject, items: T[], total: number, page: number, limit: number, hasNext: boolean) => {
+export const paginatedResponse = (set: SetObject, items: any[], total: number, page: number, limit: number, hasNext: boolean) => {
     set.status = 200;
     return successResponse(set, {
         items,
